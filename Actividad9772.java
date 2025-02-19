f4ff2c049e4170da9d6617b19afa0ebc151abe52
OnicarN <ebaudani@gmail.com>
2025-02-19 13:14:35 +0100

    cambios iniciales

---
+++ Actividad9772.java
@@ -0,0 +1,30 @@
+
+import java.util.ArrayList;
+public class Actividad9772{
+    ArrayList<String>origen;
+    int numero;
+    
+    public Actividad9772(){
+        this.origen = new ArrayList<>();
+        this.numero = 0;
+    }
+
+    public int cuantasPalabrasHayDeNLetras(ArrayList<String> origen, int numero){
+        return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
