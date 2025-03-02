49c83b31011868628161468aa9c7e34f6af2d2bc
OnicarN <ebaudani@gmail.com>
2025-03-02 19:40:52 +0100

    Contador de palabras con la misma longitud

--- Actividad9772.java
+++ Actividad9772.java
@@ -1,30 +1,24 @@
-
 import java.util.ArrayList;
 public class Actividad9772{
     ArrayList<String>origen;
     int numero;
-    
-    public Actividad9772(){
-        this.origen = new ArrayList<>();
-        this.numero = 0;
-    }
 
     public int cuantasPalabrasHayDeNLetras(ArrayList<String> origen, int numero){
-        return 0;
+        int contador;
+        int iterador = 0; 
+        int palabrasMismaLongitud = 0; 
+        int resultado = 0; 
+        if (origen != null){
+            for(String palabras: origen){
+                System.out.println(palabras);
+                contador = palabras.length();
+                if(contador == numero){
+                    palabrasMismaLongitud++;
+                    resultado = palabrasMismaLongitud;
+                }
+            }
+        }
+      return resultado;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
